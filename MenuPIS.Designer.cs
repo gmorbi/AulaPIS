b148022fb0c9061318f15a10566a2f6eed953fe2
Giovane Morbi <gemvane@msn.com>
2018-04-04 21:47:07 -0300

    Primeiro

---
+++ MenuPIS.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace AulaPIS
+{
+    partial class MenuPIS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aula3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exercicio1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exercicio2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exercicio3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exercicio4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exercicio5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Procurar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(12, 48);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(442, 20);
+            this.filePath.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aula3ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(680, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aula3ToolStripMenuItem
+            // 
+            this.aula3ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exercicio1ToolStripMenuItem,
+            this.exercicio2ToolStripMenuItem,
+            this.exercicio3ToolStripMenuItem,
+            this.exercicio4ToolStripMenuItem,
+            this.exercicio5ToolStripMenuItem});
+            this.aula3ToolStripMenuItem.Name = "aula3ToolStripMenuItem";
+            this.aula3ToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aula3ToolStripMenuItem.Text = "Aula 3";
+            // 
+            // exercicio1ToolStripMenuItem
+            // 
+            this.exercicio1ToolStripMenuItem.Name = "exercicio1ToolStripMenuItem";
+            this.exercicio1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exercicio1ToolStripMenuItem.Text = "Exercicio 1";
+            this.exercicio1ToolStripMenuItem.Click += new System.EventHandler(this.exercicio1ToolStripMenuItem_Click);
+            // 
+            // exercicio2ToolStripMenuItem
+            // 
+            this.exercicio2ToolStripMenuItem.Name = "exercicio2ToolStripMenuItem";
+            this.exercicio2ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exercicio2ToolStripMenuItem.Text = "Exercicio 2";
+            this.exercicio2ToolStripMenuItem.Click += new System.EventHandler(this.exercicio2ToolStripMenuItem_Click);
+            // 
+            // exercicio3ToolStripMenuItem
+            // 
+            this.exercicio3ToolStripMenuItem.Name = "exercicio3ToolStripMenuItem";
+            this.exercicio3ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exercicio3ToolStripMenuItem.Text = "Exercicio 3";
+            this.exercicio3ToolStripMenuItem.Click += new System.EventHandler(this.exercicio3ToolStripMenuItem_Click);
+            // 
+            // exercicio4ToolStripMenuItem
+            // 
+            this.exercicio4ToolStripMenuItem.Name = "exercicio4ToolStripMenuItem";
+            this.exercicio4ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exercicio4ToolStripMenuItem.Text = "Exercicio 4";
+            // 
+            // exercicio5ToolStripMenuItem
+            // 
+            this.exercicio5ToolStripMenuItem.Name = "exercicio5ToolStripMenuItem";
+            this.exercicio5ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exercicio5ToolStripMenuItem.Text = "Exercicio 5";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(268, 336);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Original";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(513, 413);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Processada";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(400, 74);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(268, 336);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // MenuPIS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 434);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MenuPIS";
+            this.Text = "MenuPIS";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aula3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exercicio1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exercicio2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exercicio3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exercicio4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exercicio5ToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ MenuPIS.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+
+namespace AulaPIS
+{
+    public partial class MenuPIS : Form
+    {
+        Bitmap bmpOriginal = null;
+        Bitmap bmpConvertido = null;
+        public MenuPIS()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = new OpenFileDialog();
+
+            if (result.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.FileInfo file = new System.IO.FileInfo(result.FileName);
+                filePath.Text = file.ToString();
+                pictureBox1.Image = Image.FromFile(file.ToString());
+                bmpOriginal = new Bitmap(pictureBox1.Image);
+            }
+        }
+
+        private void exercicio1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < bmpOriginal.Width; x++)
+            {
+                for (int y = 0; y < bmpOriginal.Height; y++)
+                {
+                    Color pixel = bmpOriginal.GetPixel(x, y);
+
+                    int red = pixel.R;
+                    int green = pixel.G;
+                    int blue = pixel.B;
+
+                    bmpOriginal.SetPixel(x, y, Color.FromArgb(255 - red, 255 - green, 255 - blue));
+                }
+            }
+            pictureBox2.Image = bmpOriginal;
+        }
+
+        private void exercicio2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < bmpOriginal.Width; x++)
+            {
+                for (int y = 0; y < bmpOriginal.Height; y++)
+                {
+                    Color pixel = bmpOriginal.GetPixel(x, y);
+
+                    int red = pixel.R;
+                    int green = pixel.G;
+                    int blue = pixel.B;
+
+                    bmpOriginal.SetPixel(x, y, Color.FromArgb(255 - red, 255 - green, 255 - blue));
+                }
+            }
+            pictureBox2.Image = bmpOriginal;
+        }
+
+        private void exercicio3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AForge.Imaging.Filters.RotateBilinear rotate = new AForge.Imaging.Filters.RotateBilinear(90, true);
+            //AForge.Imaging.
+            bmpConvertido = rotate.Apply(bmpOriginal);
+            pictureBox2.Image = bmpConvertido;
+        }
+    }
+}
